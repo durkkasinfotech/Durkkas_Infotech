ce482a4a61d138e0f5e6a5789dbd26fc6080204e
dkitsystem9 <dkit.system9@gmail.com>
2025-09-01 10:02:23 +0530

    vite code changed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set base path for GitHub Pages or subfolder deployment
-  base: process.env.NODE_ENV === "production" ? "/dkit-staticsite/" : "/",
+  base: "/", // or "/dkit-staticsite/" if GitHub Pages
 });
